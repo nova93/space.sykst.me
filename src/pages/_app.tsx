fdf5b977fe138462c637a5856bfe12f2e7768041
Robert Moritz <robert.moritz@levaclinic.com>
2024-06-17 21:15:31 +0200

    test of grids

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import "react-grid-layout/css/styles.css";
+import "react-resizable/css/styles.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
